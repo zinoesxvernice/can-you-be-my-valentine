219e1c92e273fd2f547572eda94f3284122be0d4
zinoesxvernice <102968619+zinoesxvernice@users.noreply.github.com>
2026-02-09 18:56:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const panel1 = document.querySelector('.panel-1');
 const panel2 = document.querySelector('.panel-2');
+const music = document.getElementById('valentine-music');
 
 panel1.addEventListener('click', () => {
+  // Play music
+  music.play();
+
   // Hide panel 1
   panel1.style.opacity = 0;
   panel1.style.pointerEvents = 'none';
